d7753e5e56820c784c266e320fde6f642692bd03
Martin Jirku <martin@jirku.sk>
2022-07-30 12:01:00 +0200

    data model for storage item

---
+++ db/004_storage_item_up.sql
@@ -0,0 +1,26 @@
+CREATE TABLE storage_items (
+    storage_item_id   INT AUTO_INCREMENT PRIMARY KEY,
+    created_at        DATETIME NOT NULL,
+    updated_at        DATETIME NOT NULL,
+    deleted_at        DATETIME NULL,
+    title             VARCHAR(255),
+    storage_place_id  INT NULL,
+    category_id       INT NULL,
+    baseline_amount   DOUBLE NOT NULL,
+    current_amount    DOUBLE NOT NULL,
+    quantity          ENUM('mass','length','volume','temperature','time','count') NOT NULL,
+    unit              VARCHAR(50) NOT NULL,
+    expiration_date   DATETIME NULL,
+
+
+    CONSTRAINT storage_item_storage_place_fk
+        FOREIGN KEY (storage_place_id)
+        REFERENCES  storage_places(storage_place_id)
+        ON DELETE   SET NULL
+        ON UPDATE   CASCADE,
+    CONSTRAINT storage_item_category_fk
+        FOREIGN KEY (category_id)
+        REFERENCES  categories(id)
+        ON DELETE   SET NULL
+        ON UPDATE   CASCADE
+);
---
+++ db/005_storage_item_consumption_up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE storage_consumptions (
+    storage_item_consumption_id INT AUTO_INCREMENT PRIMARY KEY,
+    created_at                  DATETIME NOT NULL,
+    updated_at                  DATETIME NOT NULL,
+    deleted_at                  DATETIME NULL,
+    normalized_amount           DOUBLE,
+    unit                        VARCHAR(50),
+    storage_item_id             INT NOT NULL,
+
+    CONSTRAINT storage_consumption_storage_item_fk
+        FOREIGN KEY (storage_item_id)
+        REFERENCES  storage_items(storage_item_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+)
