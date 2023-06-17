fce3736b559646f7c91d157d71b858b4f39b88be
harryjrk <martin@jirku.sk>
2023-06-17 18:43:35 +0200

    remove svelte

--- spajza/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	// redirect to login page if not logged in
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
-	onMount(() => {
-		goto('/login');
-	});
-</script>
